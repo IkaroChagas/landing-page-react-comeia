c18572b6770c1920b1091f9892e1594cfacf8b77
Ikaro <ikarochagas.ig@gmail.com>
2023-04-15 16:41:33 -0300

    second commit

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,23 +6,24 @@ function Form() {
         <>
             <div className="box4">
                 <h3>Contato</h3>
-                <form action="" method="">
-                    <div className="box5" />
-                    <label htmlFor="nome"></label>
-                    <input type="text" placeholder="Seu nome" id="nome" name="nome" required /><br>
+                <div className="box5">
+                    <form action="" method="">
+                        <label htmlFor="nome"></label>
+                        <input type="text" placeholder="Seu nome" id="nome" name="nome" required />
+                        <br />
 
                         <label htmlFor="email"></label>
-                        <input type="email" placeholder="Seu email" id="email" name="email" required /><br>
+                        <input type="email" placeholder="Seu email" id="email" name="email" required />
+                        <br />
 
-                            <label htmlFor="mensagem"></label>
-                            <textarea id="mensagem" placeholder="Digite sua mensagem" name="mensagem" required ></textarea>
-                        </br>
-                    </div>
-                    <div className="botao-enviar">
-                        <button type="submit" id="meuBotao" className="botao">Enviar</button>
-                    </div>
+                        <label htmlFor="mensagem"></label>
+                        <textarea id="mensagem" placeholder="Digite sua mensagem" name="mensagem" required ></textarea>
 
-                </form>
+                        <div className="botao-enviar">
+                            <button type="submit" id="meuBotao" className="botao">Enviar</button>
+                        </div>
+                    </form >
+                </div >
             </div>
         </>
     )
